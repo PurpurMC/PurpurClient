f7d6bffff47c75faba5565ccd7939c7123e087bb
William Blake Galbreath <Blake.Galbreath@GMail.com>
2019-12-20 17:51:48 -0600

    Inform Purpur servers about our client side mod

---
+++ src/main/java/net/pl3x/fabric/purpurclient/mixin/MixinClientPlayerEntity.java
@@ -0,0 +1,32 @@
+package net.pl3x.fabric.purpurclient.mixin;
+
+import io.netty.buffer.Unpooled;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.server.network.packet.CustomPayloadC2SPacket;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.PacketByteBuf;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayerEntity.class)
+public abstract class MixinClientPlayerEntity {
+    private static final String PROTOCOL_VERSION = "1";
+    private static final Identifier IDENTIFIER = new Identifier("purpurclient:protocol");
+
+    @Shadow
+    private String serverBrand;
+    @Shadow
+    public ClientPlayNetworkHandler networkHandler;
+
+    @Inject(at = @At("RETURN"), method = "setServerBrand")
+    public void read(CallbackInfo info) {
+        if (serverBrand != null && serverBrand.equals("Purpur")) {
+            PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer()).writeString(PROTOCOL_VERSION);
+            networkHandler.sendPacket(new CustomPayloadC2SPacket(IDENTIFIER, buf));
+        }
+    }
+}
