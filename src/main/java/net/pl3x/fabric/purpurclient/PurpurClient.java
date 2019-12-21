03275824b6ec4edb569c8eb3a39cb53a8698787d
William Blake Galbreath <Blake.Galbreath@GMail.com>
2019-12-20 18:55:34 -0600

    Fix seat position when riding bats

--- src/main/java/net/pl3x/fabric/purpurclient/PurpurClient.java
+++ src/main/java/net/pl3x/fabric/purpurclient/PurpurClient.java
@@ -1,9 +1,21 @@
 package net.pl3x.fabric.purpurclient;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.Vec3d;
 
 public class PurpurClient implements ModInitializer {
+    public static final float DEG2RAD = (float) Math.PI / 180F;
+    public static final float HALF_PI = (float) Math.PI / 2F;
+
     @Override
     public void onInitialize() {
+    }
+
+    public static void updatePassengerPosition(Entity entity, Entity passenger, double x, double y, double z, float yaw) {
+        if (entity.hasPassenger(passenger)) {
+            Vec3d offset = new Vec3d(x, y, z).rotateY(-yaw * DEG2RAD - HALF_PI);
+            passenger.setPosition(entity.getX() + offset.x, entity.getY() + offset.y, entity.getZ() + offset.z);
+        }
     }
 }
--- src/main/java/net/pl3x/fabric/purpurclient/mixin/MixinBat.java
+++ src/main/java/net/pl3x/fabric/purpurclient/mixin/MixinBat.java
@@ -3,33 +3,25 @@ package net.pl3x.fabric.purpurclient.mixin;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.passive.PolarBearEntity;
+import net.minecraft.entity.passive.BatEntity;
 import net.minecraft.world.World;
 import net.pl3x.fabric.purpurclient.PurpurClient;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 
-@Mixin(PolarBearEntity.class)
-public abstract class MixinPolarBear extends LivingEntity {
-    @Shadow
-    private float warningAnimationProgress;
-
-    public MixinPolarBear(EntityType<? extends PolarBearEntity> entityType, World world) {
+@Mixin(BatEntity.class)
+public abstract class MixinBat extends LivingEntity {
+    public MixinBat(EntityType<? extends MixinBat> entityType, World world) {
         super(entityType, world);
     }
 
     @Override
     public double getMountedHeightOffset() {
-        double height = getHeight() * 0.75D;
-        if (warningAnimationProgress > 0.0F) {
-            height -= 0.5 * (warningAnimationProgress / 6.0F);
-        }
-        return height;
+        return (double) getHeight() * 0.5D;
     }
 
     @Override
     public void updatePassengerPosition(Entity passenger) {
-        double x = warningAnimationProgress > 0.0D ? -1.0D * (warningAnimationProgress / 6.0D) : 0.0D;
+        double x = -0.25D;
         double y = getMountedHeightOffset() + passenger.getHeightOffset();
         PurpurClient.updatePassengerPosition(this, passenger, x, y, 0.0D, bodyYaw);
     }
