b3f1ecc7a4cdf5f285c66cf40bc8cde95353279b
William Blake Galbreath <Blake.Galbreath@GMail.com>
2019-12-20 21:39:07 -0600

    Fix iron golem seat height

---
+++ src/main/java/net/pl3x/fabric/purpurclient/mixin/MixinIronGolem.java
@@ -0,0 +1,28 @@
+package net.pl3x.fabric.purpurclient.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.passive.IronGolemEntity;
+import net.minecraft.world.World;
+import net.pl3x.fabric.purpurclient.PurpurClient;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(IronGolemEntity.class)
+public abstract class MixinIronGolem extends LivingEntity {
+    public MixinIronGolem(EntityType<? extends IronGolemEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Override
+    public double getMountedHeightOffset() {
+        return (double) getHeight() * 0.7D;
+    }
+
+    @Override
+    public void updatePassengerPosition(Entity passenger) {
+        double x = -0.25D;
+        double y = getMountedHeightOffset() + passenger.getHeightOffset();
+        PurpurClient.updatePassengerPosition(this, passenger, x, y, 0.0D, bodyYaw);
+    }
+}
