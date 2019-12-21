e698f6e7bca3a8af802f6e2e3f1a666b7696dbe3
William Blake Galbreath <Blake.Galbreath@GMail.com>
2019-12-20 22:26:19 -0600

    Fix parrot seat height

---
+++ src/main/java/net/pl3x/fabric/purpurclient/mixin/MixinParrot.java
@@ -0,0 +1,29 @@
+package net.pl3x.fabric.purpurclient.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.passive.CatEntity;
+import net.minecraft.entity.passive.ParrotEntity;
+import net.minecraft.world.World;
+import net.pl3x.fabric.purpurclient.PurpurClient;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ParrotEntity.class)
+public abstract class MixinParrot extends LivingEntity {
+    public MixinParrot(EntityType<? extends CatEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Override
+    public double getMountedHeightOffset() {
+        return (double) getHeight() * 0.3D;
+    }
+
+    @Override
+    public void updatePassengerPosition(Entity passenger) {
+        double x = -0.15D;
+        double y = getMountedHeightOffset() + passenger.getHeightOffset();
+        PurpurClient.updatePassengerPosition(this, passenger, x, y, 0.0D, bodyYaw);
+    }
+}
