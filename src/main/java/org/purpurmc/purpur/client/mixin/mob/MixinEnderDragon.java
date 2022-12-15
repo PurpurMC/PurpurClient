643c48059043dd0de9b2aabd6e071982aad068ee
BillyGalbreath <blake.galbreath@gmail.com>
2022-12-15 06:11:29 -0600

    Fix a couple entities using the wrong type

--- src/main/java/org/purpurmc/purpur/client/mixin/mob/MixinEnderDragon.java
+++ src/main/java/org/purpurmc/purpur/client/mixin/mob/MixinEnderDragon.java
@@ -4,14 +4,13 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.boss.dragon.EnderDragonEntity;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.passive.CatEntity;
 import net.minecraft.world.World;
 import org.purpurmc.purpur.client.entity.RidableEntity;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(EnderDragonEntity.class)
 public abstract class MixinEnderDragon extends MobEntity implements RidableEntity {
-    public MixinEnderDragon(EntityType<? extends CatEntity> entityType, World world) {
+    public MixinEnderDragon(EntityType<? extends EnderDragonEntity> entityType, World world) {
         super(entityType, world);
     }
 
--- src/main/java/org/purpurmc/purpur/client/mixin/mob/MixinParrot.java
+++ src/main/java/org/purpurmc/purpur/client/mixin/mob/MixinParrot.java
@@ -3,7 +3,6 @@ package org.purpurmc.purpur.client.mixin.mob;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.passive.CatEntity;
 import net.minecraft.entity.passive.ParrotEntity;
 import net.minecraft.world.World;
 import org.purpurmc.purpur.client.entity.RidableEntity;
@@ -11,7 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(ParrotEntity.class)
 public abstract class MixinParrot extends MobEntity implements RidableEntity {
-    public MixinParrot(EntityType<? extends CatEntity> entityType, World world) {
+    public MixinParrot(EntityType<? extends ParrotEntity> entityType, World world) {
         super(entityType, world);
     }
 
