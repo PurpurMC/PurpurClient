d9f5a7c657ff78e65bea724e9199b0d96364b04c
12emin34 <macanovic.emin@gmail.com>
2023-01-25 13:34:40 +0100

    Move player position on horse jump/buck (closes #26)

--- src/main/java/org/purpurmc/purpur/client/mixin/mob/MixinHorse.java
+++ src/main/java/org/purpurmc/purpur/client/mixin/mob/MixinHorse.java
@@ -2,10 +2,13 @@ package org.purpurmc.purpur.client.mixin.mob;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.HorseEntity;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import org.purpurmc.purpur.client.entity.RidableEntity;
+import org.purpurmc.purpur.client.mixin.accessor.AccessAbstractHorse;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(HorseEntity.class)
@@ -22,5 +25,21 @@ public abstract class MixinHorse extends MobEntity implements RidableEntity {
     @Override
     public void updatePassengerPosition(Entity passenger) {
         updatePassengerPosition(passenger, getSeats().horse);
+
+        if (passenger instanceof MobEntity mobEntity) {
+            this.bodyYaw = mobEntity.bodyYaw;
+        }
+
+        if (((AccessAbstractHorse) this).getLastAngryAnimationProgress() > 0.0F) {
+            float f = MathHelper.sin(this.bodyYaw * 0.017453292F);
+            float g = MathHelper.cos(this.bodyYaw * 0.017453292F);
+            float h = 0.7F * ((AccessAbstractHorse) this).getLastAngryAnimationProgress();
+            float i = 0.15F * ((AccessAbstractHorse) this).getLastAngryAnimationProgress();
+            passenger.setPosition(this.getX() + (h * f), this.getY() + this.getMountedHeightOffset() + passenger.getHeightOffset() + i, this.getZ() - (h * g));
+            if (passenger instanceof LivingEntity livingEntity) {
+                livingEntity.bodyYaw = this.bodyYaw;
+            }
+        }
+
     }
 }
