e2a31d26db9831f2553aebc20c70b9f9820e171c
granny <contact@granny.dev>
2024-05-02 20:27:12 -0700

    simplify blit of mob button

--- src/main/java/org/purpurmc/purpur/client/gui/screen/widget/MobButton.java
+++ src/main/java/org/purpurmc/purpur/client/gui/screen/widget/MobButton.java
@@ -1,52 +1,33 @@
 package org.purpurmc.purpur.client.gui.screen.widget;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.SpriteIconButton;
+import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.resources.ResourceLocation;
 import org.purpurmc.purpur.client.entity.Mob;
 import org.purpurmc.purpur.client.gui.screen.MobScreen;
 
 public class MobButton extends SpriteIconButton {
+    public static final int MOBS_ATLAS_WIDTH = 512;
+    public static final int MOBS_TEXTURE_WIDTH = 16;
     public static final ResourceLocation MOBS_TEXTURE = new ResourceLocation("purpurclient", "textures/mobs.png");
 
     public MobButton(Minecraft client, Screen screen, Mob mob) {
-        super(16, 16,
+        super(MOBS_TEXTURE_WIDTH, MOBS_TEXTURE_WIDTH,
             mob.getType().getDescription(),
             mob.getSpriteWidth(),
             mob.getSpriteHeight(),
             MOBS_TEXTURE,
             (button) -> client.setScreen(new MobScreen(screen, mob)), null);
+        this.setTooltip(Tooltip.create(this.getMessage()));
     }
-
-//    @Override
-//    public void renderWidget(GuiGraphics guiGraphics, int i, int j, float f) {
-//        super.renderWidget(guiGraphics, i, j, f);
-//        int k = this.getX() + this.getWidth() / 2 - this.spriteWidth / 2;
-//        int l = this.getY() + this.getHeight() / 2 - this.spriteHeight / 2;
-//        guiGraphics.blitSprite(this.sprite, k, l, this.spriteWidth, this.spriteHeight);
-//    }
 
     @Override
     public void renderWidget(GuiGraphics context, int mouseX, int mouseY, float delta) {
-        //TODO: replace with guiGraphics.blitSprite()
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderTexture(0, MOBS_TEXTURE);
-        RenderSystem.enableDepthTest();
-        context.blit(
-            MOBS_TEXTURE,
-            this.getX(),
-            this.getY(),
-            this.width * 15,
-            this.height * 14 + (this.isHoveredOrFocused() ? this.height : 0),
-            this.width,
-            this.height,
-            this.width * 16,
-            this.height * 16
-        );
+        super.renderWidget(context, mouseX, mouseY, delta);
         context.blit(
             MOBS_TEXTURE,
             this.getX(),
@@ -55,15 +36,11 @@ public class MobButton extends SpriteIconButton {
             this.spriteHeight * this.height,
             this.width,
             this.height,
-            this.width * 16,
-            this.height * 16
+            this.width * MOBS_TEXTURE_WIDTH,
+            this.height * MOBS_TEXTURE_WIDTH
         );
-        if (this.isHovered) {
-            this.renderTooltip(context, mouseX, mouseY);
-        }
     }
 
-    public void renderTooltip(GuiGraphics context, int mouseX, int mouseY) {
-        context.renderTooltip(Minecraft.getInstance().font, this.getMessage(), mouseX, mouseY);
-    }
+    @Override
+    public void renderString(GuiGraphics guiGraphics, Font font, int i) {}
 }
