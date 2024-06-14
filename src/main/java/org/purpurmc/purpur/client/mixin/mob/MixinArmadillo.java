60b25b840ee6622bff06a969bf167ad1d3881d1d
Onako2 <79749977+Onako2@users.noreply.github.com>
2024-06-14 15:33:55 +0200

    Added missing entities in the code.

---
+++ src/main/java/org/purpurmc/purpur/client/mixin/mob/MixinArmadillo.java
@@ -0,0 +1,22 @@
+package org.purpurmc.purpur.client.mixin.mob;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.animal.armadillo.Armadillo;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+import org.purpurmc.purpur.client.entity.RidableEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Armadillo.class)
+public abstract class MixinArmadillo extends Mob implements RidableEntity {
+    public MixinArmadillo(EntityType<? extends Armadillo> entityType, Level world) {
+        super(entityType, world);
+    }
+
+    @Override
+    public Vec3 getPassengerRidingPosition(Entity passenger) {
+        return super.getPassengerRidingPosition(passenger).add(getSeats().armadillo.x, getSeats().armadillo.y, getSeats().armadillo.z);
+    }
+}
