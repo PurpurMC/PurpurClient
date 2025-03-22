71ba6ee3aaaee38743d294d3c9ae0376529b08b0
granny <contact@granny.dev>
2025-03-22 15:11:34 -0700

    Revert "Remove Creaking because Minecraft doesn't allow it anyways"
    This reverts commit 4eb0c486b59402bcc19cdc970357d696839a2332.

---
+++ src/main/java/org/purpurmc/purpur/client/mixin/mob/MixinCreaking.java
@@ -0,0 +1,22 @@
+package org.purpurmc.purpur.client.mixin.mob;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.monster.creaking.Creaking;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+import org.purpurmc.purpur.client.entity.RidableEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Creaking.class)
+public abstract class MixinCreaking extends Mob implements RidableEntity {
+    public MixinCreaking(EntityType<? extends Creaking> entityType, Level world) {
+        super(entityType, world);
+    }
+
+    @Override
+    public Vec3 getPassengerRidingPosition(Entity passenger) {
+        return super.getPassengerRidingPosition(passenger).add(getSeats().creaking.x, getSeats().creaking.y, getSeats().creaking.z);
+    }
+}
